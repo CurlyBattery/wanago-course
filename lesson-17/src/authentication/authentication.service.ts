5db15ab36fe1fdb1b7bdf65468ff2b0e3591d4f3
ArtemKosyrev <artemkosyrev93@gmail.com>
2025-12-29 15:32:05 +0500

    feat: (lesson-17) add service

--- lesson-17/src/authentication/authentication.service.ts
+++ lesson-17/src/authentication/authentication.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthenticationService {
-  constructor() {}
+  constructor(private readonly usersService: UsersService) {}
+
+  register() {}
+
+  login() {}
+
+  refresh() {}
+
+  logout() {}
 }
